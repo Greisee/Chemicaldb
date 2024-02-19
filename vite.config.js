665673c5f711cf282a46d3b15d7295aef3896f10
TdgCC <85917263+TdgCC@users.noreply.github.com>
2024-02-19 00:10:25 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base:"/Chemicaldb/",
-  plugins: [react()],
-  server:{
-    host:true,
+export default defineConfig(({command})=>{
+  const config={
+    plugins: [react()],  
+    base:"/",
   }
+  if (command !== 'serve') {
+    config.base = '/react-vite-gh-pages/'
+  }
+  return config
 })
