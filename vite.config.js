08a7dd8ee27526d80444304e62598e437f3423ac
TdgCC <85917263+TdgCC@users.noreply.github.com>
2024-02-19 22:34:05 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default  config={
+export default{
   base:"/Chemicaldb/",
   plugins: [react()],  
 }
