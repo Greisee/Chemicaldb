63823e93f26b7f3b783eace9046e6a752b631f51
TdgCC <85917263+TdgCC@users.noreply.github.com>
2024-02-19 22:33:28 -0600

    idk

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import './App.css'
 
 function App() {
   return (
-    <BrowserRouter  basename={"/Chemicaldb"}>
+    <BrowserRouter>
       <Routes>
         <Route path="/Chemicaldb/" element={<Layout/>}>
           <Route index element={<MainPage/>}/>
--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({command})=>{
-  const config={
-    plugins: [react()],  
-    base:"/Chemicaldb/",
-  }
-  if (command !== 'serve') {
-    config.base = '/react-vite-gh-pages/'
-  }
-  return config
-})
+export default  config={
+  base:"/Chemicaldb/",
+  plugins: [react()],  
+}
