a3c3a1602ab8ef3a010112d6592d8f105c215eab
TdgCC <85917263+TdgCC@users.noreply.github.com>
2024-02-23 09:51:01 -0600

    resetting

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import MainPage from "./components/MainPage.jsx";
 import Layout from "./components/Layout.jsx";
 import Error404 from "./components/Error404.jsx";
@@ -7,10 +8,12 @@ import {BrowserRouter,Routes,Route} from "react-router-dom";
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <BrowserRouter basename={import.meta.env.DEV ? '/' : '/Chemicaldb/'}>
+    <BrowserRouter>
       <Routes>
-        <Route path="/Chemicaldb/" element={<Layout/>}>
+        <Route path="/" element={<Layout/>}>
           <Route index element={<MainPage/>}/>
           <Route path="ChemicalPage" element={<ChemicalPage/>}/>
           <Route path="ClassPage" element={<ClassPage/>}/>
